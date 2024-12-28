76247e1546b2377291509073399027f750966d63
amira-02 <amiraoumaymahayet@gmail.com>
2024-12-28 13:41:50 +0100

    first commit

---
+++ src/main/java/tn/pi/cabinetmedicalproject/CabinetMedicalProjectApplication.java
@@ -0,0 +1,13 @@
+package tn.pi.cabinetmedicalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinetMedicalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabinetMedicalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/pi/cabinetmedicalproject/CabinetMedicalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.pi.cabinetmedicalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabinetMedicalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
