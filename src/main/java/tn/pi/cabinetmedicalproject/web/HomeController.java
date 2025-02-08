a30be0478c1c79fb33f989d39a6e01d91b466b49
SemerNahdi <semernahdi25@gmail.com>
2025-02-08 22:13:16 +0100

    Resolved merge conflicts in HomeController.java

--- src/main/java/tn/pi/cabinetmedicalproject/web/HomeController.java
+++ src/main/java/tn/pi/cabinetmedicalproject/web/HomeController.java
@@ -1,24 +1,20 @@
 package tn.pi.cabinetmedicalproject.web;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import tn.pi.cabinetmedicalproject.model.User;
 
 @Controller
 public class HomeController {
-
 
     @GetMapping("/doctorhome")
     public String doctorHome() {
         return "doctorhome"; // Assurez-vous que le fichier doctorhome.html existe dans templates/
     }
 
-<<<<<<< HEAD
     /**
      * Méthode pour afficher la page d'accueil des pharmacies.
      * @return Le nom de la vue "pharmacyhome.html".
@@ -32,21 +28,6 @@ public class HomeController {
      * Méthode pour afficher la page d'accueil générale des patients ou visiteurs.
      * @return Le nom de la vue "Home.html".
      */
-=======
-
-//    @GetMapping("/pharmacyhome")
-//    public String pharmacyHome() {
-//        return "pharmacyhome"; // Assurez-vous que le fichier pharmacyhome.html existe dans templates/
-//    }
-
-
-//    @GetMapping("/home")
-//    public String patientHome() {
-//        return "Home"; // Assurez-vous que le fichier Home.html existe dans templates/
-//    }
-
-
->>>>>>> ed01212f1bd8e09844ed521dd39afb6fbd4221d7
     @GetMapping("/home")
     public String homePage(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -61,17 +42,4 @@ public class HomeController {
         }
         return "home"; // La vue home.html
     }
-
-<<<<<<< HEAD
-    // Uncomment and modify this method if you need a default home page
-    /*
-    @GetMapping("/")
-    public String home() {
-        // Retourner la vue avec le formulaire d'upload
-        return "upload";  // Assurez-vous que ce fichier HTML est dans le bon répertoire (par ex. src/main/resources/templates)
-    }
-    */
 }
-=======
-}
->>>>>>> ed01212f1bd8e09844ed521dd39afb6fbd4221d7
